959dfab7901cbc9196d4e70a0d815e669cef78b6
Jeremy M. Reed <reeje76@gmail.com>
2016-06-22 15:39:14 -0600

    Project setup.
    Import into Eclipse.
    Move sources to proper packages.
    Create NotImplementedException RuntimeException.
    Stub out hey(...) method.

---
+++ src/main/java/bob/Bob.java
@@ -0,0 +1,10 @@
+package bob;
+
+import exceptions.NotImplementedException;
+
+public class Bob
+{
+	public String hey(String input) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/bob/BobTest.java
+++ src/test/java/bob/BobTest.java
@@ -1,3 +1,4 @@
+package bob;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
