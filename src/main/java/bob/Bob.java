8f9f8d19592bc93ec747014f8606e36c29f8a3fe
Jeremy M. Reed <reeje76@gmail.com>
2016-06-23 09:03:17 -0600

    Add response strings.

--- src/main/java/bob/Bob.java
+++ src/main/java/bob/Bob.java
@@ -4,6 +4,11 @@ import exceptions.NotImplementedException;
 
 public class Bob
 {
+	private static final String sure = "Sure.";
+	private static final String whoa = "Whoa, chill out!";
+	private static final String fine = "Fine.  Be that way!";
+	private static final String whatever = "Whatever.";
+	
 	public String hey(String input) {
 		throw new NotImplementedException();
 	}
