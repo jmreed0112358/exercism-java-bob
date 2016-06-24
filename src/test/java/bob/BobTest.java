4b4a146b279b35d9c32c2fd6be33423e05c79e19
Jeremy M. Reed <reeje76@gmail.com>
2016-06-24 09:45:48 -0600

    Implement unit tests

--- src/test/java/bob/BobTest.java
+++ src/test/java/bob/BobTest.java
@@ -6,6 +6,131 @@ import static org.junit.Assert.*;
 public class BobTest {
     private final Bob bob = new Bob();
 
+    
+    // sanitize(...) tests.
+    // isQuestion(...) tests.
+    @Test
+    public void test_isQuestion_NullString_ThrowsException() {
+    	try {
+    		bob.isQuestion( null );
+    		fail("Supposed to catch NullPointerException");
+    	} catch ( NullPointerException npe ) {
+    		
+    	} catch ( Exception e ) {
+    		fail("Expected to catch NullPointerException");
+    	}
+    }
+    
+    @Test 
+    public void test_isQuestion_EmptyString_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "" ) );
+    }
+    
+    @Test 
+    public void test_isQuestion_AllSpacesString_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "   " ) );
+    }
+    
+    @Test
+    public void test_isQuestion_NoLettersAllSymbolsNoQuestionMark_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "^&*^*&&^*&^*&^*(()" ) );
+    }
+    
+    @Test
+    public void test_isQuestion_NoLettersAllSymbolsQuestionMark_ReturnsTrue() {
+    	assertTrue( bob.isQuestion( "^&*^*&&^*&^*&^*(()!?" ) );
+    }
+    
+    @Test
+    public void test_isQuestion_Question_ReturnsTrue() {
+    	assertTrue( bob.isQuestion( "This is a question?" ) );
+    }
+    
+    @Test
+    public void test_isQuestion_Statement_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "This is a statement." ) );
+    }
+    
+    @Test
+    public void test_isQuestion_Forceful_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "This is a forceful statement!" ) );
+    }
+    
+    // isYelling(...) tests.
+    @Test
+    public void test_isYelling_NullString_ThrowsException() {
+    	try {
+    		bob.isYelling( null );
+    		fail("Supposed to catch NullPointerException");
+    	} catch ( NullPointerException npe ) {
+    		
+    	} catch ( Exception e ) {
+    		fail("Expected to catch NullPointerException");
+    	}
+    }
+    
+    @Test 
+    public void test_isYelling_EmptyString_ReturnsFalse() {
+    	assertFalse( bob.isYelling( "" ) );
+    }
+    
+    @Test
+    public void test_isYelling_AllSpacesString_ReturnsFalse() {
+    	assertFalse( bob.isYelling( "   " ) );
+    }
+    
+    @Test
+    public void test_isYelling_AllLettersAreCapsExclamationMark_ReturnsTrue() {
+    	assertTrue( bob.isYelling( "SHUT UP!" ) );
+    }
+    
+    @Test
+    public void test_isYelling_AllLettersAreCapsNoExclamationMark_ReturnsTrue() {
+    	assertTrue( bob.isYelling( "SHUT UP" ) );
+    }
+    
+    @Test
+    public void test_isYelling_AllButOneLetterAreCapsExclamationMark_ReturnsFalse() {
+    	assertFalse( bob.isYelling( "No YOU FOOL!" ) );
+    }
+    
+    @Test
+    public void test_isYelling_AllButOneLetterAreCapsNoExclamationMark_ReturnsFalse() {
+    	assertFalse( bob.isYelling( "No YOU FOOL" ) );
+    }
+    
+    @Test
+    public void test_isQuestion_NoLettersAllSymbolsNoExclamationMark_ReturnsFalse() {
+    	assertFalse( bob.isQuestion( "^&*^*&&^*&^*&^*(()" ) );
+    }
+    
+    @Test
+    public void test_isQuestion_NoLettersAllSymbolsExclamationMark_ReturnsTrue() {
+    	assertTrue( bob.isQuestion( "^&*^*&&^*&^*&^*(()?!" ) );
+    }
+    
+    // isAllSpaces(...) tests.
+    @Test
+    public void test_isEmptyOrAllSpaces_EmptyString_ReturnsTrue() {
+    	assertTrue( bob.isEmptyOrAllSpaces( "" ) );
+    }
+    
+    @Test
+    public void test_isEmptyOrAllSpaces_SingleSpace_ReturnsTrue() {
+    	assertTrue( bob.isEmptyOrAllSpaces( "" ) );
+    }
+    
+    @Test
+    public void test_isEmptyOrAllSpaces_MultipleSpace_ReturnsTrue() {
+    	assertTrue( bob.isEmptyOrAllSpaces( "" ) );
+    }
+    
+    @Test
+    public void test_isEmptyOrAllSpaces_NotAllSpaces_ReturnsFalse() {
+    	assertFalse( bob.isEmptyOrAllSpaces( "    foo  " ) );
+    }
+    
+    // hey(...) tests.
     @Test
     public void test_hey_NullString_ThrowsException() {
     	try {
@@ -16,6 +141,11 @@ public class BobTest {
     	} catch ( Exception e ) {
     		fail("Expected to catch NullPointerException");
     	}
+    }
+    
+    @Test
+    public void test_hey_ShoutingButNotAllLettersAreCaps_ReturnsExpectedResponse() {
+    	assertEquals( "Whatever", bob.hey( "No YOU FOOL!" ) );
     }
     
     @Test
@@ -120,7 +250,7 @@ public class BobTest {
             "Whoa, chill out!", bob.hey("I HATE YOU")
         );
     }
-
+    
     @Test
     public void statementContainingQuestionMark() {
         assertEquals(
